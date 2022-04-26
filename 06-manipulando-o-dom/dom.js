150ebe8412badca575633cc53b504f151f4dac6e
Palloma Hortencio <palloma.hortencio@gmail.com>
2022-04-26 10:36:08 -0300

    js

--- 06-manipulando-o-dom/dom.js
+++ 06-manipulando-o-dom/dom.js
@@ -29,3 +29,27 @@ legenda.innerHTML = "<b>Ilustração do DOM</b>";
 titulo.style.textAlign = "center"; 
 titulo.style.backgroundImage = "linear-gradient(lightyellow, lightblue)";
 titulo.style.color = "red";
+
+const listaEditores = document.querySelector('#lista-editores');
+
+const ultimo = listaEditores.querySelector('li:last-child');
+const primeiro = listaEditores.querySelector('li:first-child');
+const outro = listaEditores.querySelector('li:nth-child(2)');
+
+outro.innerHTML = "Deu ruim";
+console.error(ultimo);
+
+outro.style.color = "red";
+
+ultimo.classList.add('destaque-item');
+
+
+// adicionando o atributo target em todos os links da lista de referencias
+// selecionando todos os links contidos na lista ul
+const links = document.querySelectorAll('ul li a');
+console.log(links);
+links[0].style.color = "red";
+
+for(let i = 0; i < links.length; i++) {
+    links[i].setAttribute('target', '_blank')
+}
---
+++ 07-funcoes/funcoes.js
@@ -0,0 +1,26 @@
+/* sintaxe 1 (tradicional)
+função literal/anonima */
+let exemplo1 = function() {
+    console.log("Função anônima / literal");
+};
+
+// chamando a função
+exemplo1();
+
+
+/* sintaxe 2 (tradicional)
+função nomeada */
+function exemplo2() {
+    console.log("Função nomeada");
+}
+exemplo2(); // chamando a função
+
+
+
+
+
+
+
+
+/* sintaxe 3 (moderna)
+função seta/flecha (arrow function) */
