0a32388571ad2e89a0d54c48ee0a078b13bed4ef
Palloma Hortencio <palloma.hortencio@gmail.com>
2022-04-26 12:00:53 -0300

    exercicio

--- 07-funcoes/funcoes.js
+++ 07-funcoes/funcoes.js
@@ -16,11 +16,58 @@ function exemplo2() {
 exemplo2(); // chamando a função
 
 
-
-
-
-
-
-
 /* sintaxe 3 (moderna)
 função seta/flecha (arrow function) */
+let exemplo3 = () => {
+    console.log('Arrow function');
+};
+exemplo3(); // chamando a função
+
+let exemplo3b = () => console.log("Arrow function B");
+exemplo3b();
+
+//Exemplos
+function dobra(valor) {
+   return valor * 2;
+}
+document.write(dobra(10)); 
+
+// Arrow Function
+let dobraB = (valor) => {
+    return valor * 2;
+}
+console.log(dobraB(100));
+
+
+let dobraC = valor => valor * 100;
+console.log(dobraC(20)); 
+
+console.log('----------');
+
+/* Exemplos anteriores */
+let preco = 1250;
+let desconto = 77.88;
+let precoFinal = preco - desconto;
+
+function formataMoeda(valor) {
+    return valor.toLocaleString('en', {
+        style: 'currency',
+        currency: 'USD'
+    })
+}
+
+// arrow
+let formataValor = valor => {
+    return valor.toLocaleString('pt-br', {
+        style: 'currency',
+        currency: 'BRL'
+    })
+}
+
+console.log(formataValor(preco));
+
+console.log(formataValor(preco));
+console.log(formataMoeda(desconto));
+console.log(formataMoeda(precoFinal));
+
+console.log('----------');
---
+++ 08-eventos/eventos.js
@@ -0,0 +1,6 @@
+let nome = document.getElementById("nome");
+let resultado = document.getElementById("resultado");
+
+function clique() {
+    resultado.textContent = nome.value;
+}
